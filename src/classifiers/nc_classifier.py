76d0cc8adb9328a009d5647ea2b0ef2810894787
pcatrinck <108846665+pcatrinck@users.noreply.github.com>
2023-01-30 12:46:05 -0300

    Update nc_classifier.py

--- src/classifiers/nc_classifier.py
+++ src/classifiers/nc_classifier.py
@@ -9,8 +9,22 @@ class NearestCentroidClassifier(ClassifierInterface):
         super().__init__()
 
     def train(self, train_dataset: DatasetInterface) -> None:
-        """ calcular os centroides por classe """
+        #lista de classes
+        dict_class = {}
+        for i in range (train_dataset.size):
+            amostra, classe = train_dataset[i].get[1]
+            if classe not in dict_class:
+                dict_class[classe] = []
+            dict_class[classe].append(amostra)
+
+            #calcular os centroides por classe
+            #para isso, guardar o vetor junto do item da amostra
+            tam = len(amostra)
+        
 
     def predict(self, test_dataset: DatasetInterface) -> List[str]:
         """ para cada amostra no dataset, buscar o centroide mais proximo e respectiva retornar a classe """
+
         return []
+
+    
